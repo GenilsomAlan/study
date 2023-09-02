d64f1421c39c06905768ed2703cd0f140625d0fc
Genilsom Alan <allanbb072@gmail.com>
2023-09-02 18:30:29 -0300

    separação do modulo createCardVideos.js

---
+++ script.js
@@ -0,0 +1,2 @@
+import { CARDVIDEOS } from "./createCardVideos.js";
+var cardPrograming = new CARDVIDEOS("python","Playlist de programação na linguagem python3","../imgs/javascript.jpg","Capa ilustrativa Python")
