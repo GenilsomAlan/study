a8da31824969d9bda375268c6470e60a181dac21
Genilsom Alan <allanbb072@gmail.com>
2023-09-03 16:09:38 -0300

    laço para criar os cards dinamicamente, e arrayss de dados

--- createCardVideos.js
+++ createCardVideos.js
@@ -1,28 +1,13 @@
-/*
-<div class="listVideos">
-    <div class="cardVideos">
-        <div class="img">
-            <img src="../imgs/javascript.jpg" alt="/">
-        </div>
-        <div class="title">
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-        </div>
-        <div class="subTitle">
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
-        </div>
-    </div>
-</div>
-*/
-
-class CARDVIDEOS{
+export class CARDVIDEOS{
     constructor(title,subTitle,img,altImg){
         this.title = title;
         this.subTitle = subTitle;
         this.img = img;
         this.altImg = altImg;
     }
-    blockCardVideos = () => {
-        return `<div class="cardVideos">
+    createCard(){
+        var listVideos = document.querySelector(".listVideos")
+        listVideos.innerHTML += `<div class="cardVideos">
             <div class="img">
                 <img src="${this.img}" alt="${this.altImg}">
             </div>
@@ -36,9 +21,3 @@ class CARDVIDEOS{
     }
 }
 
-const createCard = () =>{
-    var main = document.querySelector(".listVideos")
-    var cardPrograming = new CARDVIDEOS("python","Playlist de programação na linguagem python3","../imgs/javascript.jpg","Capa ilustrativa Python")
-    main.innerHTML = cardPrograming.blockCardVideos()    
-}
-createCard()
--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 import { CARDVIDEOS } from "./createCardVideos.js";
-var cardPrograming = new CARDVIDEOS("python","Playlist de programação na linguagem python3","../imgs/javascript.jpg","Capa ilustrativa Python")
+
+var titles = ["Python","JavaScript","Php","HTML","CSS"]
+var subTitle = ["Playlist de programação na linguagem python3","Playlist de programação na linguagem JavaScript",
+    "Playlist de programação na linguagem Php","Playlist de programação na linguagem HTML","Playlist de programação na linguagem CSS"]
+var imgsURL = ["../imgs/python.png","../imgs/javascript.jpg","../imgs/php.png","../imgs/html.png","../imgs/css.png"]
+var textAlt = ["Capa ilustrativa Python","Capa ilustrativa Javascript","Capa ilustrativa Php","Capa ilustrativa HTML","Capa ilustrativa CSS"]
+
+function showCard(i){
+    var cardPrograming = new CARDVIDEOS(titles[i],subTitle[i],imgsURL[i],textAlt[i])
+    cardPrograming.createCard()
+}
+var i = 0
+for(i; i < titles.length; i++){
+    showCard(i)
+}
